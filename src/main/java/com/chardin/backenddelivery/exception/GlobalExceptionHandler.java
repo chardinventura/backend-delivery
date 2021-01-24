979580689d2994cb4a2dd68ebf8444c1c431519d
Chardin De La Cruz Ventura <chardindelacruzventura829@gmail.com>
2021-01-24 13:16:02 -0400

    handleMethodArgumentNotValidException replace by handleValidationException.

--- src/main/java/com/chardin/backenddelivery/exception/GlobalExceptionHandler.java
+++ src/main/java/com/chardin/backenddelivery/exception/GlobalExceptionHandler.java
@@ -1,12 +1,9 @@
 package com.chardin.backenddelivery.exception;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -30,18 +27,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(body, notFound);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity handleValidationException(ValidationException validationException) {
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
 
         ValidationResponse body = new ValidationResponse();
         body.setMessage(status.getReasonPhrase());
         body.setStatus(status.value());
         body.setTimeStamp(LocalDateTime.now());
 
-        Map<String, String> errors = ex.getBindingResult()
+        Map<String, String> errors = validationException.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .collect(Collectors.toMap(
@@ -50,7 +46,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         body.setErrors(errors);
 
-        return new ResponseEntity<>(body, headers, status);
+        return new ResponseEntity<>(body, status);
     }
-
 }
