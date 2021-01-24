a4c03f0e1d68f2447cfbbfc6e7e575e8ab1fd05d
Chardin De La Cruz Ventura <chardindelacruzventura829@gmail.com>
2021-01-24 13:18:10 -0400

    ValidationException was add to handle validation exceptions.

---
+++ src/main/java/com/chardin/backenddelivery/exception/ValidationException.java
@@ -0,0 +1,20 @@
+package com.chardin.backenddelivery.exception;
+
+import org.springframework.validation.BindingResult;
+
+public class ValidationException extends RuntimeException{
+
+    private BindingResult bindingResult;
+
+    public ValidationException(BindingResult bindingResult) {
+        this.bindingResult = bindingResult;
+    }
+
+    public BindingResult getBindingResult() {
+        return bindingResult;
+    }
+
+    public void setBindingResult(BindingResult bindingResult) {
+        this.bindingResult = bindingResult;
+    }
+}
