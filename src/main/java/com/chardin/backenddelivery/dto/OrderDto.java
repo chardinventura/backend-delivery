2b2caa26db7755cf6bf40b9e9cd39c2054e19d55
Chardin <chardindelacruzventura829@gmail.com>
2021-04-03 05:38:07 -0400

    Getters and setters methods were changed by lombok, orders property of UserDto was ignored.

--- src/main/java/com/chardin/backenddelivery/dto/OrderDto.java
+++ src/main/java/com/chardin/backenddelivery/dto/OrderDto.java
@@ -1,6 +1,8 @@
 package com.chardin.backenddelivery.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.ToString;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
@@ -8,6 +10,8 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@ToString(exclude = {"id"})
 public class OrderDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -22,64 +26,6 @@ public class OrderDto implements Serializable {
     private String address;
     @JsonIgnoreProperties("order")
     private List<ProductDto> products;
-    private UserDto userDto;
-
-    public OrderDto() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getOrderTime() {
-        return orderTime;
-    }
-
-    public void setOrderTime(LocalDateTime orderTime) {
-        this.orderTime = orderTime;
-    }
-
-    public Float getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(Float totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public List<ProductDto> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<ProductDto> products) {
-        this.products = products;
-    }
-
-    public UserDto getUserDto() {
-        return userDto;
-    }
-
-    public void setUserDto(UserDto userDto) {
-        this.userDto = userDto;
-    }
+    @JsonIgnoreProperties("orders")
+    private UserDto user;
 }
