0b5bf38c26c72c26c145b064113c1c3919d67420
Chardin <chardindelacruzventura829@gmail.com>
2021-04-03 05:47:22 -0400

    Unused methods were removed.

--- src/main/java/com/chardin/backenddelivery/repository/UserRepository.java
+++ src/main/java/com/chardin/backenddelivery/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.chardin.backenddelivery.repository;
 
-import com.chardin.backenddelivery.dto.UserDto;
 import com.chardin.backenddelivery.entity.User;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,8 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);
-	Boolean existsByUsernameOrEmailOrPhone(String username, String email, String phone);
 	Boolean existsByUsername(String username);
 	Boolean existsByPhone(String phone);
-	Boolean existsByEmail(String email);
 }
