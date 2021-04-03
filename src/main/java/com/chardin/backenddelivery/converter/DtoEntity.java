e9e776e314d43f5bca597d7464db853b7ced76b6
Chardin <chardindelacruzventura829@gmail.com>
2021-04-03 05:41:55 -0400

    Methods were refactored to an overload.

--- src/main/java/com/chardin/backenddelivery/converter/DtoEntity.java
+++ src/main/java/com/chardin/backenddelivery/converter/DtoEntity.java
@@ -21,105 +21,64 @@ public class DtoEntity {
 	@Autowired
 	private ModelMapper modelmapper;
 
-	//User-UserDto
-	public UserDto getUserDto(User user) {
-
+	// to dto
+	public UserDto toDto(User user) {
 		return user != null ? modelmapper.map(user, UserDto.class) : null;
 	}
 
-	public User getUser(UserDto userDto) {
-
-		return userDto != null ? modelmapper.map(userDto, User.class) : null;
-	}
-
-	public List<UserDto> getUsersDto(List<User> users) {
-
-		return users.stream().map(u -> modelmapper.map(u, UserDto.class)).collect(Collectors.toList());
-	}
-
-	public List<User> getUsers(List<UserDto> usersDto) {
-
-		return usersDto.stream().map(u -> modelmapper.map(u, User.class)).collect(Collectors.toList());
-	}
-
-	//Rol-RolDto
-	public AuthorityDto getAuthorityDto(Authority authority) {
-
+	public AuthorityDto toDto(Authority authority) {
 		return authority != null ? modelmapper.map(authority, AuthorityDto.class) : null;
 	}
 
-	public Authority getAuthority(AuthorityDto authorityDto) {
-
-		return authorityDto != null ? modelmapper.map(authorityDto, Authority.class) : null;
-	}
-
-	public List<AuthorityDto> getAuthoritiesDto(List<Authority> authorities) {
-
-		return authorities
-				.stream()
-				.map(r -> modelmapper.map(r, AuthorityDto.class))
-				.collect(Collectors.toList());
-	}
-
-	public List<Authority> getAuthorities(List<AuthorityDto> authoritiesDto) {
-
-		return authoritiesDto
-				.stream()
-				.map(r -> modelmapper.map(r, Authority.class))
-				.collect(Collectors.toList());
-	}
-	
-	//Order
-	public OrderDto getOrderDto(Order order) {
-
+	public OrderDto toDto(Order order) {
 		return order != null ? modelmapper.map(order, OrderDto.class) : null;
 	}
 
-	public Order getOrder(OrderDto orderDto) {
-
-		return orderDto != null ? modelmapper.map(orderDto, Order.class) : null;
-	}
-
-	public List<OrderDto> getOrdersDto(List<Order> orders) {
-
-		return orders
-				.stream()
-				.map(r -> modelmapper.map(r, OrderDto.class))
-				.collect(Collectors.toList());
-	}
-
-	public List<Order> getOrders(List<OrderDto> ordersDto) {
-
-		return ordersDto
-				.stream()
-				.map(r -> modelmapper.map(r, Order.class))
-				.collect(Collectors.toList());
-	}
-
-	//Product
-	public ProductDto getProductDto(Product product) {
-
+	public ProductDto toDto(Product product) {
 		return product != null ? modelmapper.map(product, ProductDto.class) : null;
 	}
 
-	public Product getProduct(ProductDto productDto) {
+	// to Entity
+	public User toEntity(UserDto userDto) {
+		return userDto != null ? modelmapper.map(userDto, User.class) : null;
+	}
 
+	public Authority toEntity(AuthorityDto authorityDto) {
+		return authorityDto != null ? modelmapper.map(authorityDto, Authority.class) : null;
+	}
+
+	public Order toEntity(OrderDto orderDto) {
+		return orderDto != null ? modelmapper.map(orderDto, Order.class) : null;
+	}
+
+	public Product toEntity(ProductDto productDto) {
 		return productDto != null ? modelmapper.map(productDto, Product.class) : null;
 	}
 
-	public List<ProductDto> getProductsDto(List<Product> products) {
-
-		return products
-				.stream()
-				.map(r -> modelmapper.map(r, ProductDto.class))
+	// to dtoList
+	public List<UserDto> toUsersDto(List<User> users) {
+		return users.stream()
+				.map(this::toDto)
 				.collect(Collectors.toList());
 	}
 
-	public List<Product> getProducts(List<ProductDto> productsDto) {
+	public List<AuthorityDto> toAuthoritiesDto(List<Authority> authorities) {
+		return authorities.stream()
+				.map(this::toDto)
+				.collect(Collectors.toList());
+	}
 
-		return productsDto
+	public List<OrderDto> toOrdersDto(List<Order> orders) {
+		return orders
 				.stream()
-				.map(r -> modelmapper.map(r, Product.class))
+				.map(this::toDto)
+				.collect(Collectors.toList());
+	}
+
+	public List<ProductDto> toProductsDto(List<Product> products) {
+		return products
+				.stream()
+				.map(this::toDto)
 				.collect(Collectors.toList());
 	}
 }
